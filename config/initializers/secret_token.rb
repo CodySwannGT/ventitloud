8b8f354fa243d4be089b454b8025be2a88123d05
Cody Swann <cody@gunnertech.com>
2013-03-04 07:26:49 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ventitloud::Application.config.secret_token = '43659f4c37b7370fed4964a2e53f9edc5c3e7c0e3ef08f4092119bbe782cd0027a1c78f6a62acf6febc199454adb318efd2c1837048113b0971548d5b6120d8f'
